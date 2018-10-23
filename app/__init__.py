1fbf87ae63fa617d3d43e6038493320e359dfcf2
n0lean <cpy19960205@zju.edu.cn>
2018-10-23 15:50:06 -0400

    user home, db, auth, api, register, login

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
 
     if test_config is None:
@@ -21,5 +22,15 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import home
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
---
+++ app/api.py
@@ -0,0 +1,1 @@
+# implement home rest api here
