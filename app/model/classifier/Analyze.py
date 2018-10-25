72bffac6b3b17d1cd16b4cea8af7f865ed3aefaa
yoongbok lee <js951204@naver.com>
2018-10-25 00:57:51 -0400

    Update Analyze.py

--- app/model/classifier/Analyze.py
+++ app/model/classifier/Analyze.py
@@ -2,6 +2,8 @@ from classifier import *
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 import re
+
+
 def Analyze_content(str):
     html = urlopen(str)
     soup = BeautifulSoup(html.read())
@@ -10,11 +12,13 @@ def Analyze_content(str):
         string = " ".join(re.split("[^a-zA-Z.,!?]*", string.lower())).strip()
         data.append(string)
     return get_tweets_predictions(data)
+
+
 if __name__ == '__main__':
     result = Analyze_content("https://en.wikipedia.org/wiki/Twitter")
     print result
-    count = 0;
+    count = 0
     for i in result:
-       count = count+1;
+       count = count+1
        if i!=2:
-           print i,count;
+           print i,count
